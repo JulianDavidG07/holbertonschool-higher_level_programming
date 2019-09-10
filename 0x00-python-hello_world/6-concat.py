d4543888f2257f64f3e80b9c41f9938456e489a1
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-09-10 06:57:31 +0000

    concat

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str3 = "!"
-print("Welcome to {} {}{}".format(str1[0:9], str2[0:6], str3[0]))
+print("Welcome to {} {}!".format(str1[0:9], str2[0:6]))
+

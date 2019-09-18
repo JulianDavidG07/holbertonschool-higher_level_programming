710749b8622834d8dd71d8b9b3050c8a85e3523c
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-09-17 20:29:05 -0500

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return(None)
-    for i in range(0, len(matrix)):
-        print(*matrix[i])
+    for i in range(len(matrix)):
+        print("{} {} {}".format(*matrix[i] * 3))

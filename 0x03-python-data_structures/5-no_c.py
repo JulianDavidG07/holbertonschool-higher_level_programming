cec0f511dc74ef0ae84863eea885a6a64cc10e10
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-09-17 19:08:54 -0500

    no_cC

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+def no_c(my_string):
+    letters = 'cC'
+    delete = ''
+    for char in my_string:
+        if char not in letters:
+            delete = delete + char
+            return(delete)

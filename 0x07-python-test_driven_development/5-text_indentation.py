7fb284480d951dc9dd4d2961c6a7d5c20d53e78e
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-10-11 17:22:24 -0500

    identation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -15,7 +15,9 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     text = text.replace(". ", ".\n\n")
-    text = text.replace(".", "\n")
+    text = text.replace(".", ".\n")
     text = text.replace("? ", "?\n\n")
+    text = text.replace("?", "?\n")
     text = text.replace(": ", ":\n\n")
+    text = text.replace(":", ":\n")
     print(text)

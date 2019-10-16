ad032c8e025511a2f43672a943c939639a6b9aa0
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-10-15 20:57:14 -0500

    kind_of_class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+instance class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+    return true if object is instance class
+    '''
+    return isinstance(obj, a_class)

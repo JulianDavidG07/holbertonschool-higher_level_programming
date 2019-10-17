a1cc788da90b7901afbd6486db77980e8f8800a6
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-10-16 19:14:56 -0500

    json_format

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+
+'''
+funcion return JSON format
+'''
+
+
+def to_json_string(my_obj):
+    '''
+    object represented in JSON format
+    '''
+    return json.dumps(my_obj)

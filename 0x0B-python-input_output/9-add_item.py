ad9b568d7f0687e4cc399c8dd5fc79237fc678fe
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-10-17 01:02:33 -0500

    add_item

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -6,6 +6,7 @@ Python function that add arguments to a Python list
 from sys import argv
 
 save_to_json_file = __import__('7-save_to_json_file.py').save_to_json_file
+
 load_from_json_file = __import__('8-load_from_json_file.py').load_from_json_file
 
 my_list = []
@@ -20,5 +21,3 @@ for args in range(1, len(argv)):
     n_list.append(argv[args])
 
 save_to_json_file(n_list, "add_item.json")
-
-

fb6c013f2cc51a16516dd53cd8b1549e8d6a2628
Julian David Gaitan Saenz <julygsaenz1995@hotmail.com>
2019-11-27 15:02:26 -0500

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,6 @@
--- script that creates the table id_not_null on your MySQL server.
-SELECT
-FROM
-ORDER BY
-IGNORE ALL;
+-- MySQL to list all shows and genres linked
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name ASC;

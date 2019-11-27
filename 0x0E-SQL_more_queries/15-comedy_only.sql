2e57376135bfbb4ea41e9b809b9dd5eaf6391bc7
Julian David Gaitan Saenz <julygsaenz1995@hotmail.com>
2019-11-27 15:01:44 -0500

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,7 @@
--- script that creates the table id_not_null on your MySQL server.
-SELECT
-FROM
-ORDER BY
-IGNORE ALL;
+-- MySql script to select all titles that are comedy
+SELECT title FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;

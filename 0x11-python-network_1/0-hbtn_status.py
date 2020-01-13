a17e236c19bfee7a7a93691ae9f575e604a0438d
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-01-13 17:02:08 -0500

    hbtn school

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # Python script that fetches https://intranet.hbtn.io/status
 import urllib.request
-opener = urllib.request.FancyURLopener({})
-with opener.open("https://intranet.hbtn.io/status") as f:
+
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as f:
 
     url = f.read()
     print('Body response:')
@@ -10,4 +10,3 @@ with opener.open("https://intranet.hbtn.io/status") as f:
           '\t- utf8 content: {}'.format(type(url),
                                        url,
                                        url.decode('utf-8')))
-if __name__ == "__main__":

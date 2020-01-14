5df38aee99fdc7e0f668fb5785719f67a4a522f1
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-01-13 23:02:33 -0500

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,11 +4,9 @@ Write a Python script that fetches https://intranet.hbtn.io/status
 """
 import requests
 
-if __name__ == "__main__":
+with requests.get("https://intranet.hbtn.io/status") as f:
 
-    with requests.get("https://intranet.hbtn.io/status") as f:
-
-        url = f.text
-        print('Body response:\n\t- type: {}\n\t'
-              '- content: {}'.format(type(url),
+    url = f.text
+    print('Body response:\n\t- type: {}\n\t'
+          '- content: {}'.format(type(url),
                                      url,))

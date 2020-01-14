382004d150cba6806ae2ecea14f20955ab364fc2
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-01-14 01:17:37 -0500

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """
 Write a Python script that takes in a URL,
-sends a request to the URL and displays
-the value of the variable X-Request-Id in the response header
+sends a request to the URL and displays the body of the response.
 """
 import requests
+from requests.exceptions import HTTPError
 import sys
 
 if __name__ == "__main__":
-    pass
+    try:
+        response = requests.get(sys.argv[1])
+        response.raise_for_status()
+        print(response.text)
+
+    except HTTPError as err:
+        print('Error code: {}'.format(err.response.status_code))

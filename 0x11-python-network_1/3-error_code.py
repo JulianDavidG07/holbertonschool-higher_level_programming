dfe6841cdfcd240a9c0d44e1449e3cdb3f6a471e
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-01-13 21:56:54 -0500

    error-code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL, sends a request to 
+the URL and displays the body of the response (decoded in utf-8).
+"""
+from urllib.request import Request, urlopen
+from urllib.request import URLError, HTTPError
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        req = Request(argv[1])
+        response = urlopen(req)
+        print(response.read().decode())
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))

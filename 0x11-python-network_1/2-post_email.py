cf01e12927514d055425987aae7d40c01df9c6e9
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-01-13 21:01:01 -0500

    post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
 
     email = {'email': argv[2]}
     data = urllib.parse.urlencode(email)
-    data = data.encode('ascii')
     r = urllib.request.Request(argv[1], data)
     with urllib.request.urlopen(r) as f:
         print(str(f.read(), 'utf-8'))

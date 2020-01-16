2847a62ee03125f21079a3b845d1b68764085aa6
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-01-15 20:54:26 -0500

    github

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a URL,
-sends a request to the URL and displays
-the value of the variable X-Request-Id in the response header
+Python script that takes 2 arguments in order to solve this challenge.
+
+The first argument will be the repository name
+The second argument will be the owner name
+You must use the packages requests and sys
 """
 import requests
 import sys
 
 if __name__ == "__main__":
-    pass
+
+    url = f'https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits'
+    response = requests.get(url)
+
+for i in range(10):
+    commit = (response.json()[i].get('sha'))
+    author = (response.json()[i].get('commit').get('author').get('name'))
+    print(f'{commit}: {author}')

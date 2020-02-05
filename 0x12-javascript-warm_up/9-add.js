9331f23cdb76e3bd1207b9de2f6b9436f2503f27
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-04 23:22:46 -0500

    [FIX] add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // Script that prints the addition of 2 integers
 
-const arg = process.argv.slice(2)
+const arg = process.argv.slice(2);
 
 function add (a, b) {
-  return parseInt(arg[0]) + parseInt(arg[1])
+  return parseInt(arg[0]) + parseInt(arg[1]);
 }
 
-console.log(add(arg[0], arg[1]))
+console.log(add(arg[0], arg[1]));

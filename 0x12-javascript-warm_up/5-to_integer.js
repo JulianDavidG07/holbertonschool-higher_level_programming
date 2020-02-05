d9ae9dc692c82fd871da6d74222a564a49b6f863
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-04 23:26:28 -0500

    [FIX] to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 // JS to check if arguments passed cam be converted to integer
 
 if (isNaN(process.argv[2])) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else {
-  console.log('My number:', process.argv[2])
+  console.log('My number:', process.argv[2]);
 }

527581794265b5357e99ef5dd3e9b9fe6810ef02
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-04 23:24:56 -0500

    [FIX] multly

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop.
 
-const languages = ['C is fun', 'Python is cool', 'Javascript is amazing']
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (const element in languages) {
-  console.log(languages[element])
+  console.log(languages[element]);
 }

b8b5c0bace4fd52465653466633d84892ed98216
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-05 15:42:50 -0500

    [UPDATE] correction standard style

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-// script that prints x times “C is fun”
-
-const x = process.argv.slice(2)
-const c = 'C is fun\n'
-
-switch (x[0]) {
-  case x[0] > 0:
-    console.log(c.repeat(x[0]))
-    break
-  case x[0] === 0:
-    console.log('Missing number of occurrences')
-    break
-  case x[0] < 0:
-    // pass
+const arg = process.argv.slice(2)
+let i = 0
+const num = parseInt(Number(arg[0]))
+if (num) {
+  while (i < num) {
+    console.log('C is fun')
+    i++
+  }
+} else {
+  console.log('Missing number of occurrences')
 }

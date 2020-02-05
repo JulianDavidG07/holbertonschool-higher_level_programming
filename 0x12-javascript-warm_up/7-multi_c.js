e0971d3ce21d953a95814879f9b6bd31bc6ad556
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-05 15:45:44 -0500

    [UPDATE] correction standard style

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const arg = process.argv.slice(2)
-let i = 0
-const num = parseInt(Number(arg[0]))
+const arg = process.argv.slice(2);
+let i = 0;
+const num = parseInt(Number(arg[0]));
 if (num) {
   while (i < num) {
-    console.log('C is fun')
+    console.log('C is fun');
     i++
   }
 } else {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 }

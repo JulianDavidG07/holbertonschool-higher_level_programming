2a5666ee714fceeb043af2ef716143804b34a922
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-04 23:29:49 -0500

    [UPDATE] concatenate

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Script that prints two arguments passed to it, in the following format: “ is ”
 
-let myArg = process.argv.slice(2);
+const myArg = process.argv.slice(2);
 
 console.log(`${myArg[0]} is ${myArg[1]}`);

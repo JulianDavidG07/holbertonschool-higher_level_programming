07268c9abbfc6a9d4c3c9f212d7d98440a38a231
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-05 07:44:34 -0500

    [FIX] second-max array

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that searches the second biggest integer in the list of arguments.
+
+const myArg = process.argv.slice(2);
+
+if ((myArg.length === 0) || (myArg.length === 1)) {
+  console.log('0');
+} else {
+  console.log(myArg.sort().reverse()[1]);
+}

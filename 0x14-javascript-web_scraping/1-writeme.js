fffb3e934de543abda8924efccd6647c2ab984af
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-11 12:08:48 -0500

    [UPDATE] standar checks

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // Script that writes a string to a file
 
-const fs = require('fs')
+const fs = require('fs');
 
-const content = process.argv[3]
+const content = process.argv[3];
 
 try {
-  const data = fs.writeFileSync(process.argv[2], content)
+  const data = fs.writeFileSync(process.argv[2], content);
   // file written successfully
 } catch (err) {
-  console.error(err)
+  console.error(err);
 }

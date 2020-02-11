e941caabbb68fbc27b29f6986589ed11740099d0
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-11 11:02:48 -0500

    [FIX] Write in File

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Script that writes a string to a file
+
+const fs = require('fs')
+
+const content = process.argv[3]
+
+try {
+  const data = fs.writeFileSync(process.argv[2], content)
+  // file written successfully
+} catch (err) {
+  console.error(err)
+}

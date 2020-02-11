28af6a06a9ba9da5aa238816e5cdb43ee0bc3a5f
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-11 12:41:31 -0500

    [FIX] status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that display the status code of a GET request
+
+const request = require('request');
+
+request
+  .get(process.argv[2])
+  .on('response', (response) => {
+    console.log(`code: ${response.statusCode}`);
+  });

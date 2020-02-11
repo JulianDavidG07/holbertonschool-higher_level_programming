d837054449b59c5350e09baf9ef1e02a67928fb3
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-11 16:51:27 -0500

    [UPDATE] else add

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
-    return
+    console.log(err);
+  } else {
+    console.log(data);
   }
-  console.log(data);
 })

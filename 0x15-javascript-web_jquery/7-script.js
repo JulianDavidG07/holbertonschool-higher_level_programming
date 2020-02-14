4627ad3f6712c51f4d27420f8417c029d795cb55
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-14 00:29:43 -0500

    [FIX] getJson jquery

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,5 @@
 // Javascript script that fetches and replaces the name of this URL: https://swapi.co/api/people/5/?format=json
 
-$.getJSON('https://swapi.co/api/people/5/?format=json', function(json) {
+$.getJSON('https://swapi.co/api/people/5/?format=json', function (json) {
   $('DIV#character').text(json.name);
 });

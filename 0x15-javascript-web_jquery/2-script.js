684729a79dbb578723a5d9655d379fb319e9363f
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-13 22:33:42 -0500

    [FIX] click jquery

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,4 @@
+// Javascript script that updates the text color of the HTML tag HEADER to red (#FF0000) when the user clicks on the tag DIV#red_header
+$('#red_header').click(function () {
+  $('HEADER').css('color', '#FF0000'); 
+});

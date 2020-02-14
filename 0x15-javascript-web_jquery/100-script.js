4b4ab6b554b7b17e051b73aa4d265e886472ad0f
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-02-14 13:42:21 -0500

    [FIX] querySelector Vainilla JS

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// Javascript script that updates the text color of the HTML tag HEADER to red (#FF0000)
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('HEADER').style.color = 'FF0000';
+});
